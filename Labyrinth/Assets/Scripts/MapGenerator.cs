b6e84f4b3ef1eb7277974907d68f66afe5c96f94
Legion123 <hagiu.stefan@yahoo.com>
2017-07-18 14:40:44 +0300

    I added the initialization for the map generator

---
+++ Labyrinth/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+	const int unitialized = 0;
+	const float almostOne = (float) 0.9999999;
+
+	int height;
+	int width;
+	const int defaultHeight = 10;
+	const int defaultWidth = 10;
+	public int startingX; // I consider that the X coordinate represents height and they Y coordinate represents width
+	public int startingY;
+
+	public enum cellType {wall, clear};
+	public List < List <cellType> > map = new List < List <cellType> >(); //the map is going to be represented as a matrix of bools
+
+	void Awake () {
+		
+		height = PlayerPrefs.GetInt ("height"); 
+		width = PlayerPrefs.GetInt ("width");
+
+		if (height == unitialized && width == unitialized) {
+			height = defaultHeight;
+			width = defaultWidth;
+		}
+			
+		if (startingX == unitialized && startingY == unitialized) {
+			startingX = (int) Random.Range (1, height + almostOne);
+			startingY = (int) Random.Range (1, width + almostOne);
+		}
+
+		for (int i = 0; i <= height; i++) {
+			map.Add (new List <cellType> () );
+			for (int j = 0; j <= width; j++) {
+				map [i].Add (cellType.wall);
+			}
+		}
+		map [startingX] [startingY] = cellType.clear;
+	}
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
