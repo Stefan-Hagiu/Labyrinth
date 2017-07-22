64f4ce605e9e158764700b1135f572af6d8e3d02
Stefan-Hagiu <hagiu.stefan@yahoo.com>
2017-07-22 17:11:33 +0300

    Added camera animation

--- Labyrinth/Assets/Scripts/CameraMovement.cs
+++ Labyrinth/Assets/Scripts/CameraMovement.cs
@@ -25,6 +25,27 @@ public class CameraMovement : MonoBehaviour {
 	public int currentCellPositionX;
 	public int currentCellPositionY;
 
+	float currentCameraPositionX;
+	float currentCameraPositionY;
+	float finalCameraPositionX;
+	float finalCameraPositionY;
+
+	float min (float a, float b) {
+		if (a < b) {
+			return a;
+		} else {
+			return b;
+		}
+	}
+
+	float max (float a, float b) {
+		if (a >b) {
+			return a;
+		} else {
+			return b;
+		}
+	}
+
 	void placeplayerCell () {
 		playerCell = Instantiate (cell);
 		playerCell.transform.Translate (50 * startingCellPositionY, -50 * startingCellPositionX, 0);
@@ -39,10 +60,16 @@ public class CameraMovement : MonoBehaviour {
 		endingCell.transform.SetParent (canvas.transform, false);
 	}
 
+	void placeCamera() {
+		this.gameObject.transform.Translate (50 * startingCellPositionY, -50 * startingCellPositionX, 0);
+		currentCameraPositionX = finalCameraPositionX = 50 * startingCellPositionY;
+		currentCameraPositionY = finalCameraPositionY = -50 * startingCellPositionX;
+	}
+
 	void Start () {
 		placeplayerCell ();
 		placeendingCell ();
-		this.gameObject.transform.Translate (50 * startingCellPositionY, -50 * startingCellPositionX, 0);
+		placeCamera ();
 		currentCellPositionX = startingCellPositionX;
 		currentCellPositionY = startingCellPositionY;
 		mapHeight = map.Count - 2;
@@ -62,7 +89,7 @@ public class CameraMovement : MonoBehaviour {
 
 	void tryMoveLeft () {
 		if (map [currentCellPositionX] [currentCellPositionY - 1] == SharedDataTypes.cellType.clear) {
-			this.gameObject.transform.Translate (-50, 0, 0);
+			finalCameraPositionX -= 50;
 			playerCell.transform.Translate (-50, 0, 0);
 			currentCellPositionY--;
 		}
@@ -70,7 +97,7 @@ public class CameraMovement : MonoBehaviour {
 
 	void tryMoveRight () {
 		if (map [currentCellPositionX] [currentCellPositionY + 1] == SharedDataTypes.cellType.clear) {
-			this.gameObject.transform.Translate (50, 0, 0);
+			finalCameraPositionX += 50;
 			playerCell.transform.Translate (50, 0, 0);
 			currentCellPositionY++;
 		}
@@ -78,7 +105,7 @@ public class CameraMovement : MonoBehaviour {
 
 	void tryMoveDown () {
 		if (map [currentCellPositionX + 1] [currentCellPositionY] == SharedDataTypes.cellType.clear) {
-			this.gameObject.transform.Translate (0, -50, 0);
+			finalCameraPositionY -= 50;
 			playerCell.transform.Translate (0, -50, 0);
 			currentCellPositionX++;
 		}
@@ -86,7 +113,7 @@ public class CameraMovement : MonoBehaviour {
 
 	void tryMoveUp () {
 		if (map [currentCellPositionX - 1] [currentCellPositionY] == SharedDataTypes.cellType.clear) {
-			this.gameObject.transform.Translate (0, 50, 0);
+			finalCameraPositionY += 50;
 			playerCell.transform.Translate (0, 50, 0);
 			currentCellPositionX--;
 		}
@@ -125,9 +152,29 @@ public class CameraMovement : MonoBehaviour {
 		}
 	}
 
+	void animateCamera () {
+		if (currentCameraPositionX < finalCameraPositionX) {
+			this.gameObject.transform.Translate (max ((finalCameraPositionX - currentCameraPositionX) / 10, 0.1f), 0, 0);
+			currentCameraPositionX = this.gameObject.transform.position.x;
+		}
+		if (currentCameraPositionX > finalCameraPositionX) {
+			this.gameObject.transform.Translate (min ((finalCameraPositionX - currentCameraPositionX) / 10, -0.1f), 0, 0);
+			currentCameraPositionX = this.gameObject.transform.position.x;
+		}
+		if (currentCameraPositionY < finalCameraPositionY) {
+			this.gameObject.transform.Translate (0, max ((finalCameraPositionY - currentCameraPositionY) / 10, 0.1f), 0);
+			currentCameraPositionY = this.gameObject.transform.position.y;
+		}
+		if (currentCameraPositionY > finalCameraPositionY) {
+			this.gameObject.transform.Translate (0, min ((finalCameraPositionY - currentCameraPositionY) / 10, -0.1f), 0);
+			currentCameraPositionY = this.gameObject.transform.position.y;
+		}
+	}
+
 	void Update () {
 		checkZoom ();
 		checkMovement ();
 		checkFinish ();
+		animateCamera ();
 	}
 }
